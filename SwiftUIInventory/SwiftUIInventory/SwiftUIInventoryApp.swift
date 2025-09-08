8ccf8e9330d23873803ffd3708b94203d7cf886a
Alexander <alexander.hsiao@ditto.live>
2025-09-08 12:27:49 -0400

    Initial commit

---
+++ SwiftUIInventory/SwiftUIInventory/SwiftUIInventoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIInventoryApp.swift
+//  SwiftUIInventory
+//
+//  Created by Alexander on 2025-09-08.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIInventoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIInventory/SwiftUIInventoryTests/SwiftUIInventoryTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIInventoryTests.swift
+//  SwiftUIInventoryTests
+//
+//  Created by Alexander on 2025-09-08.
+//
+
+import Testing
+@testable import SwiftUIInventory
+
+struct SwiftUIInventoryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
